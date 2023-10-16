cde20083e6c87a20256dfb196f8f7c138df5deef
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-10-17 01:19:59 +0530

    feat: model created

---
+++ models/bookModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model("BookStore", bookSchema);
